ab804fc348faf982dc4a4e5ac5688d4f4b19a055
Misaf <misaf.1990@gmail.com>
2026-01-28 01:56:59 +0330

    Refactor Doc params

--- src/SmsGatewayManager.php
+++ src/SmsGatewayManager.php
@@ -12,30 +12,24 @@ final class SmsGatewayManager extends Manager
 {
     /**
      * Get the default SMS Gateway driver name.
-     *
-     * @return string
      */
-    public function getDefaultDriver()
+    public function getDefaultDriver(): string
     {
         return $this->config->get('sms_gateway.default');
     }
 
     /**
      * Create an instance of the Ghasedak driver.
-     *
-     * @return \Drivers\Ghasedak
      */
-    protected function createGhasedakDriver()
+    protected function createGhasedakDriver(): GhasedakDriver
     {
         return new GhasedakDriver();
     }
 
     /**
      * Create an instance of the Sunway driver.
-     *
-     * @return \Drivers\Ghasedak
      */
-    protected function createSunwayDriver()
+    protected function createSunwayDriver(): SunwayDriver
     {
         return new SunwayDriver();
     }
